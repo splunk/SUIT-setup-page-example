70131fa7845acba6836cbeca3ca5d3b95a510378
Nusair Haq <nhaq@splunk.com>
2022-05-05 18:17:14 -0400

    first commit

---
+++ packages/setup-example-app/src/main/webapp/pages/start/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import layout from '@splunk/react-page';
+import SetupComponent from '@splunk/setup-component';
+import { SplunkThemeProvider } from '@splunk/themes';
+
+import { defaultTheme, getThemeOptions } from '@splunk/splunk-utils/themes';
+
+import { StyledContainer, StyledGreeting } from './StartStyles';
+
+const themeProviderSettings = getThemeOptions(defaultTheme() || 'enterprise');
+
+layout(
+    <SplunkThemeProvider {...themeProviderSettings}>
+        <StyledContainer>
+            <SetupComponent />
+        </StyledContainer>
+    </SplunkThemeProvider>
+);
